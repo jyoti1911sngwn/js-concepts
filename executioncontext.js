82612c5cd01da1d6abce1045ee2e8ff5008c53d2
jyoti sangwan <jyotisangwan@jyotis-Air.lan>
2025-11-30 07:49:56 +0530

    hoisting

--- executioncontext.js
+++ executioncontext.js
@@ -3,5 +3,5 @@ function square(num){
     var ans = num* num;
     return ans;
 }
-var square1 = square(1)
-var square2 = square(2)
+var square1 = square(n)
+var square2 = square(4)
---
+++ hoisting.js
@@ -0,0 +1,6 @@
+var n = 2
+function fun (){
+    console.log("helo world")
+}
+fun()
+console.log(n)
